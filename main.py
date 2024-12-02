40d7caaa2e9564b5598ffcbef7ff5808a63bcef2
Justin Graham <justin.graham@imanage.com>
2024-12-01 19:22:41 -0600

    move tree image to center of page

--- main.py
+++ main.py
@@ -23,21 +23,27 @@ with ui.tab_panels(tab, value=december_first).classes("w-full"):
         day_of_december = i + 1
         tab = tabs[day_of_december]
         with ui.tab_panel(tab).style("background-color:#0e0f23;"):
-            data = ui.input(f"data for day {day_of_december}").style("color:white;")
-            ui.button(
-                "part one results",
-                on_click=lambda d=day_of_december, u=data: aoc_utils.get_results(
-                    d, u, 1
-                ),  # Use default argument to capture `data`
-            ).style('color: #0e0f23;')
-            ui.button(
-                "part two results",
-                on_click=lambda d=day_of_december, u=data: aoc_utils.get_results(
-                    d, u, 2
-                ),  # Use default argument to capture `data`
-            ).style('color: #0e0f23;')
-            christmas_tree_image_path = aoc_utils.get_christmas_tree_image_path()
-            ui.image(christmas_tree_image_path).style("width: 600px; height: 600px;")
+            data = ui.input(label="Paste Data Here").props('input-style="color: white"')
+            with ui.row():
+                ui.button(
+                    "part one results",
+                    on_click=lambda d=day_of_december, u=data: aoc_utils.get_results(
+                        d, u, 1
+                    ),  # Use default argument to capture `data`
+                ).style('color: #0e0f23;')
+                ui.button(
+                    "part two results",
+                    on_click=lambda d=day_of_december, u=data: aoc_utils.get_results(
+                        d, u, 2
+                    ),  # Use default argument to capture `data`
+                ).style('color: #0e0f23;')
+
+                for _ in range(15):
+                    ui.space()
+
+                christmas_tree_image_path = aoc_utils.get_christmas_tree_image_path()
+                ui.image(christmas_tree_image_path).style("width: 600px; height: 600px;")
+
 
 ui.add_head_html(style_sheet)
 
